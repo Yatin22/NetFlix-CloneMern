afd00414b948edc62fda5e2cc4385a1e205675c3
Yatin22 <yatinjhamb10@gmail.com>
2023-02-22 23:16:43 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const userRoutes = require("./routes/UserRoutes");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb+srv://trail1:jhyat123456@trailcluster.fnethod.mongodb.net/try-netlflix?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+app.use("/api/user", userRoutes);
+
+app.listen(5001, () => {
+  console.log("server started on port 5001");
+});
